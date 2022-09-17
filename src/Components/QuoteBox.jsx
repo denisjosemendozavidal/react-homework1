ca5867daf3b1376bfa2f9f243fa691f7fdbb5e30
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-09-17 08:43:48 -0500

    Uploading project

--- src/Components/QuoteBox.jsx
+++ src/Components/QuoteBox.jsx
@@ -7,8 +7,10 @@ const QuoteBox = ({randomQuote, randomColor, getRandomAll}) => {
     
     return (
         <article className='card'>
-            <i className="fa-solid fa-quote-left" style={{color: randomColor}}></i>
-            <p className='card_quote' style={{color: randomColor}}>{`"${randomQuote.quote}"`}</p>
+            <div className='quoteandquotations'>
+                <i className="fa-solid fa-quote-left" style={{color: randomColor}}></i>
+                <p className='card_quote' style={{color: randomColor}}>{`"${randomQuote.quote}"`}</p>
+            </div>
             <h1 className='card_author' style={{color: randomColor}}>{randomQuote.author}</h1>
             <QuoteBoxButton
                 randomQuote={randomQuote} 
